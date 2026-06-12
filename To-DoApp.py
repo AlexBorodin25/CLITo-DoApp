700e29620a3f042a035041e21731b8602d418ae1
alexb <aborod1@wgu.edu>
2026-06-12 13:50:15 -0400

    Added complete_tasks function

--- To-DoApp.py
+++ To-DoApp.py
@@ -13,12 +13,33 @@ def save_tasks(tasks):
         for task in tasks:
             f.write(task+"\n")
 
-def add_task():
-    task = input("Enter new task: ")
-    tasks.append(task)
-    print("Task added")
+def complete_task(tasks):
+    view_tasks(tasks)
+    if not tasks:
+        return
+    try:
+        choice = int(input("Enter the task number to complete."))
+        if 0 < choice <= len(tasks):
+            if tasks[choice-1].startwith("[ ]"):
+                tasks[choice-1] = tasks[choice-1].replace("[ ]", "[x]", 1)
+                save_tasks(tasks)
+                print("Task completed")
+            else:
+                print("Invalid choice")
+    except ValueError:
+        print("Enter a valid number")
 
-def view_task():
+
+def add_task(tasks):
+    task = input("Enter new task: ").strip()
+    if task:
+        tasks.append(task)
+        save_tasks(tasks)
+        print("Task added")
+    else:
+         print("Invalid task")
+
+def view_tasks(tasks):
     if len(tasks) == 0:
         print("No tasks")
     else:
@@ -26,7 +47,7 @@ def view_task():
         for i, task in enumerate(tasks):
             print(f"{i+1}. {task}")
 
-def delete_task():
+def delete_task(tasks):
     if len(tasks) == 0:
         print("No tasks")
     else:
@@ -36,9 +57,11 @@ def delete_task():
         choice = int(input("Enter the task number."))
         if 0 < choice <= len(tasks):
             del tasks[choice-1]
+            save_tasks(tasks)
             print("Task deleted")
         else:
             print("Invalid choice")
+
 
 def main():
     while True:
@@ -46,16 +69,19 @@ def main():
         print("1. Add task")
         print("2. View task")
         print("3. Delete task")
-        print("4. Exit")
+        print("4. Complete task")
+        print("5. Exit")
 
         choice = int(input("Enter your choice."))
         if choice == 1:
-            add_task()
+            add_task(tasks)
         elif choice == 2:
-            view_task()
+            view_tasks(tasks)
         elif choice == 3:
-            delete_task()
+            delete_task(tasks)
         elif choice == 4:
+            complete_task(tasks)
+        elif choice == 5:
             print("Thank you for using the To-Do App")
             break
         else:
